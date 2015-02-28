fc4a7e68cd9df52e1c6f9250cfd3848d9aeef2c9
ok2ju <soulSpringg@gmail.com>
2015-02-28 22:57:42 +0300

    syntax fix

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,42 +1,42 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
-        postcss: {
-            options: {
-                processors: [
-                    require('autoprefixer-core')({
-                        browsers: ['last 2 version', 'ie 8', 'ie 9']
-                    }).postcss,
-                    require('csswring').postcss
-                ]
-            },
-            dist: {
-                src: 'stylesheets/*.css'
-            }
-        },
+    postcss: {
+      options: {
+        processors: [
+          require('autoprefixer-core')({
+            browsers: ['last 2 version', 'ie 8', 'ie 9']
+          }).postcss,
+          require('csswring').postcss
+        ]
+      },
+      dist: {
+        src: 'stylesheets/*.css'
+      }
+    },
 
-        sass: {
-            dist: {
-                options: {
-                    sourcemap: 'none'
-                },
-                files: {
-                    'stylesheets/style.css': 'sass/style.scss'
-                }
-            }
+    sass: {
+      dist: {
+        options: {
+          sourcemap: 'none'
         },
-
-        watch: {
-            postcss: {
-                files: ['**/*.css'],
-                tasks: ['postcss']
-            },
-            sass: {
-                files: ['**/*.{scss,sass}'],
-                tasks: ['sass']
-            }
+        files: {
+          'stylesheets/style.css': 'sass/style.scss'
         }
-    });
+      }
+    },
+
+    watch: {
+      postcss: {
+        files: ['**/*.css'],
+        tasks: ['postcss']
+      },
+      sass: {
+        files: ['**/*.{scss,sass}'],
+        tasks: ['sass']
+      }
+    }
+  });
 
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-sass');
